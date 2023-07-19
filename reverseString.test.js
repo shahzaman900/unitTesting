ce217cd88362f8931b15017d15839dde05dfca2b
sheezu900 <shahzamanlive2@gmail.com>
2023-07-19 10:53:18 +0500

    learn testing

--- reverseString.test.js
+++ reverseString.test.js
@@ -1,5 +1,5 @@
 const reverseString = require('./unittest');
 
 test('should return reverse string of "Shah" ', () => {
-  expect(reverseString('Shah')).toMatch('hahS');
+  expect(reverseString('Shah')).toMatch<String>('hahS');
 });
